6fe449af6e656eb210e0caaf518d3f74443399f1
Larissa S <larissafschiavo@gmail.com>
2018-01-07 02:41:56 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length
-    yield(collection[i])
+    block_return_values << yield(collection[i])
     i = i + 1
   end
 
@@ -12,5 +12,5 @@ def my_all?(collection)
     false
   else
     true
-  end 
+  end
 end
